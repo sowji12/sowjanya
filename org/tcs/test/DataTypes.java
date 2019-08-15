bf537296d9658feba617462cfaf32972a4fa76c5
sowji <sowjanyag873@gmail.com>
2019-08-15 11:16:58 +0530

    new code added for datatype

---
+++ org/tcs/test/DataTypes.java
@@ -0,0 +1,26 @@
+package org.tcs.test;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		short emId=2345;
+		System.out.println("emId is"+emId);
+		String eName="ssss";
+		System.out.println("eNmae is"+eName);
+		String eMail="sowji@gmail.com";
+		System.out.println("eMail is"+eMail);
+		long emPhNO=9980485432l;
+		System.out.println("emPhNO is"+emPhNO);
+		Float salary=234.453f;
+		System.out.println("salary is"+salary);
+		String Gender="female";
+		System.out.println("Gender is"+Gender);
+		String city="chennai";
+		System.out.println("city is"+city);
+	
+		
+
+	}
+	
+	}
+
+
